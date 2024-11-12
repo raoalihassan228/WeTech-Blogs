4adec1bed4e046b9e5792d4ea940f9f9c812572f
Sahand Ghavidel <sahand.gha@gmail.com>
2024-11-12 18:10:49 +0700

    Complete the search page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,9 +7,30 @@ import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import { dark, light } from '@clerk/themes';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 export default function Header() {
   const path = usePathname();
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchParams = useSearchParams();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [searchParams]);
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -21,12 +42,14 @@ export default function Header() {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
