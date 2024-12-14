48f9f43999a006e32d3373291e15382e126f05fc
Ali Hassan <raoalihassan1145@gmail.com>
2024-12-14 22:14:01 +0500

    firebase integrated

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-bac5b.firebaseapp.com",
-  projectId: "next-blog-bac5b",
-  storageBucket: "next-blog-bac5b.firebasestorage.app",
-  messagingSenderId: "282459795765",
-  appId: "1:282459795765:web:9392a74d8014b012c2dc7c"
+  authDomain: "wetech-blog.firebaseapp.com",
+  projectId: "wetech-blog",
+  storageBucket: "wetech-blog.firebasestorage.app",
+  messagingSenderId: "556108573382",
+  appId: "1:556108573382:web:5cc2dbd8784f5d4b9ac5c7"
 };
 
 // Initialize Firebase
