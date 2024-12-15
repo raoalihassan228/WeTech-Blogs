bd313cce7ed6327fc3cedb3f00c4e89bdb3dd1ac
Ali Hassan <raoalihassan1145@gmail.com>
2024-12-15 12:56:07 +0500

    updated name

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           WeTech
         </span>
-        Blog
+        Blogs
       </Link>
       <form onSubmit={handleSubmit}>
         <TextInput
